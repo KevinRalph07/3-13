f2570760c0180dea17d824939764a2ce325950cd
Kevin <kevin.ralph@smoothstack.com>
2023-07-24 17:24:24 -0500

    added game manager class

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,16 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     // Attributes
-    public GameObject TheDeck;
-    private List<Card> mHand;
 
-    public Player()
-    {
-        mHand = new List<Card>();
-    }
 
     // Start is called before the first frame update
     void Start()
