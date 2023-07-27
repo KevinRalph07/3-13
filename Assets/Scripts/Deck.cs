0614ea41bd106e33534a151ddff66a4dd3b0d3bd
Kevin <kevin.ralph@smoothstack.com>
2023-07-27 16:14:43 -0500

    Added art assets and got meshes on the deck and discard pile

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Data;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
@@ -8,11 +9,14 @@ public class Deck : MonoBehaviour
 {
     // Attributes
     public GameObject Card;
+    public GameObject DiscardPileObject;
     private int mNumDecks;
     private int mNumJokers;
     private List<Card> mFullDeck;
     private Stack<Card> mCurDeck;
     private Stack<Card> mCurDiscardPile;
+
+    public Dictionary<Suit, Dictionary<int, Vector2>> CardTextureCoordinates;
 
 
     // Constructor
@@ -26,6 +30,51 @@ public class Deck : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        InitCardTextureCoordDict();
+
+        Suit suit = Suit.SPADES;
+        int value = 2;
+
+        int x = (int)CardTextureCoordinates[suit][value][0];
+        int y = (int)CardTextureCoordinates[suit][value][1];
+        int width = 148;
+        int height = 198;
+        int textureWidth = 1332;
+        int textureHeight = 1188;
+
+        Vector3[] vertices = new Vector3[4];
+        Vector2[] uv = new Vector2[4];
+        int[] triangles = new int[6];
+
+        vertices[0] = new Vector3(0, 1);
+        vertices[1] = new Vector3(1, 1);
+        vertices[2] = new Vector3(0, 0);
+        vertices[3] = new Vector3(1, 0);
+
+        //uv[0] = new Vector2(0, 1);
+        //uv[1] = new Vector2(1, 1);
+        //uv[2] = new Vector2(0, 0);
+        //uv[3] = new Vector2(1, 0);
+
+        uv[0] = ConvertPixelsToUVCoordinates(x, y + height, textureWidth, textureHeight);
+        uv[1] = ConvertPixelsToUVCoordinates(x + width, y + height, textureWidth, textureHeight);
+        uv[2] = ConvertPixelsToUVCoordinates(x, y, textureWidth, textureHeight);
+        uv[3] = ConvertPixelsToUVCoordinates(x + width, y, textureWidth, textureHeight);
+
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+        triangles[3] = 2;
+        triangles[4] = 1;
+        triangles[5] = 3;
+
+        GameObject back = DiscardPileObject.transform.GetChild(0).gameObject;
+        MeshFilter filter = back.GetComponent<MeshFilter>();
+        filter.mesh.vertices = vertices;
+        filter.mesh.uv = uv;
+        filter.mesh.triangles = triangles;
+
+        //DiscardPileObject.transform.localScale = new Vector3(DiscardPileObject.transform.localScale.x, 0.002f, DiscardPileObject.transform.localScale.z);
         
     }
 
@@ -33,6 +82,11 @@ public class Deck : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public Vector2 ConvertPixelsToUVCoordinates(int x, int y, int textureWidth, int textureHeight)
+    {
+        return new Vector2((float)x / textureWidth, (float)y / textureHeight);
     }
 
     public List<Card> InitDeck(int numDecks, int numJokers)
@@ -88,9 +142,34 @@ public class Deck : MonoBehaviour
         return mCurDeck;
     }
 
-    public Stack<Card> CurDeck { get { return mCurDeck; } }
+    private void InitCardTextureCoordDict()
+    {
+        CardTextureCoordinates = new Dictionary<Suit, Dictionary<int, Vector2>>();
 
-    public Stack<Card> DiscardPile {  get { return mCurDiscardPile; } }
+        Dictionary<int, Vector2> joker = new Dictionary<int, Vector2>();
+        Dictionary<int, Vector2> spades = new Dictionary<int, Vector2>();
+        Dictionary<int, Vector2> hearts = new Dictionary<int, Vector2>();
+        Dictionary<int, Vector2> clubs = new Dictionary<int, Vector2>();
+        Dictionary<int, Vector2> diamonds = new Dictionary<int, Vector2>();
+
+
+        spades[2] = new Vector2(740, 0);
+
+        joker[1] = new Vector2(1184, 0);
+
+
+        CardTextureCoordinates[Suit.JOKER] = joker;
+        CardTextureCoordinates[Suit.SPADES] = spades;
+        CardTextureCoordinates[Suit.HEARTS] = hearts;
+        CardTextureCoordinates[Suit.CLUBS] = clubs;
+        CardTextureCoordinates[Suit.DIAMONDS] = diamonds;
+
+
+    }
+
+    public Stack<Card> TheDeck { get { return mCurDeck; } }
+
+    public Stack<Card> TheDiscardPile {  get { return mCurDiscardPile; } }
 
 
 }
