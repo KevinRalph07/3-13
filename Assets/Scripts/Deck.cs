d60b299d1b0d9e8a107bf151ccf20d38f8b9ef5a
Kevin <kevin.ralph@smoothstack.com>
2023-07-30 15:17:00 -0500

    start of game is 'working'. create deck, shuffle, deal and display player 1 ui

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -27,54 +27,14 @@ public class Deck : MonoBehaviour
         mCurDiscardPile = new Stack<Card>();
     }
 
+    private void Awake()
+    {
+        InitCardTextureCoordDict();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        InitCardTextureCoordDict();
-
-        Suit suit = Suit.SPADES;
-        int value = 2;
-
-        int x = (int)CardTextureCoordinates[suit][value][0];
-        int y = (int)CardTextureCoordinates[suit][value][1];
-        int width = 148;
-        int height = 198;
-        int textureWidth = 1332;
-        int textureHeight = 1188;
-
-        Vector3[] vertices = new Vector3[4];
-        Vector2[] uv = new Vector2[4];
-        int[] triangles = new int[6];
-
-        vertices[0] = new Vector3(0, 1);
-        vertices[1] = new Vector3(1, 1);
-        vertices[2] = new Vector3(0, 0);
-        vertices[3] = new Vector3(1, 0);
-
-        //uv[0] = new Vector2(0, 1);
-        //uv[1] = new Vector2(1, 1);
-        //uv[2] = new Vector2(0, 0);
-        //uv[3] = new Vector2(1, 0);
-
-        uv[0] = ConvertPixelsToUVCoordinates(x, y + height, textureWidth, textureHeight);
-        uv[1] = ConvertPixelsToUVCoordinates(x + width, y + height, textureWidth, textureHeight);
-        uv[2] = ConvertPixelsToUVCoordinates(x, y, textureWidth, textureHeight);
-        uv[3] = ConvertPixelsToUVCoordinates(x + width, y, textureWidth, textureHeight);
-
-        triangles[0] = 0;
-        triangles[1] = 1;
-        triangles[2] = 2;
-        triangles[3] = 2;
-        triangles[4] = 1;
-        triangles[5] = 3;
-
-        GameObject back = DiscardPileObject.transform.GetChild(0).gameObject;
-        MeshFilter filter = back.GetComponent<MeshFilter>();
-        filter.mesh.vertices = vertices;
-        filter.mesh.uv = uv;
-        filter.mesh.triangles = triangles;
-
-        //DiscardPileObject.transform.localScale = new Vector3(DiscardPileObject.transform.localScale.x, 0.002f, DiscardPileObject.transform.localScale.z);
         
     }
 
@@ -142,6 +102,53 @@ public class Deck : MonoBehaviour
         return mCurDeck;
     }
 
+    public void UpdateDiscardPileImage(Card card)
+    {
+        Suit suit = card.Suit;
+        int value = card.Value;
+
+        int x = (int)CardTextureCoordinates[suit][value][0];
+        int y = (int)CardTextureCoordinates[suit][value][1];
+        int width = 148;
+        int height = 198;
+        int textureWidth = 1332;
+        int textureHeight = 1188;
+
+        Vector3[] vertices = new Vector3[4];
+        Vector2[] uv = new Vector2[4];
+        int[] triangles = new int[6];
+
+        vertices[0] = new Vector3(0, 1);
+        vertices[1] = new Vector3(1, 1);
+        vertices[2] = new Vector3(0, 0);
+        vertices[3] = new Vector3(1, 0);
+
+        //uv[0] = new Vector2(0, 1);
+        //uv[1] = new Vector2(1, 1);
+        //uv[2] = new Vector2(0, 0);
+        //uv[3] = new Vector2(1, 0);
+
+        uv[0] = ConvertPixelsToUVCoordinates(x, y + height, textureWidth, textureHeight);
+        uv[1] = ConvertPixelsToUVCoordinates(x + width, y + height, textureWidth, textureHeight);
+        uv[2] = ConvertPixelsToUVCoordinates(x, y, textureWidth, textureHeight);
+        uv[3] = ConvertPixelsToUVCoordinates(x + width, y, textureWidth, textureHeight);
+
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+        triangles[3] = 2;
+        triangles[4] = 1;
+        triangles[5] = 3;
+
+        GameObject back = DiscardPileObject.transform.GetChild(0).gameObject;
+        MeshFilter filter = back.GetComponent<MeshFilter>();
+        filter.mesh.vertices = vertices;
+        filter.mesh.uv = uv;
+        filter.mesh.triangles = triangles;
+
+        //DiscardPileObject.transform.localScale = new Vector3(DiscardPileObject.transform.localScale.x, 0.002f, DiscardPileObject.transform.localScale.z);
+    }
+
     private void InitCardTextureCoordDict()
     {
         CardTextureCoordinates = new Dictionary<Suit, Dictionary<int, Vector2>>();
@@ -152,11 +159,63 @@ public class Deck : MonoBehaviour
         Dictionary<int, Vector2> clubs = new Dictionary<int, Vector2>();
         Dictionary<int, Vector2> diamonds = new Dictionary<int, Vector2>();
 
+        //spades[2] = new Vector2(740, 0);
+        spades[1] = new Vector2(148 * 4, 198 * 0);
+        spades[2] = new Vector2(148 * 5, 198 * 0);
+        spades[3] = new Vector2(148 * 6, 198 * 0);
+        spades[4] = new Vector2(148 * 7, 198 * 0);
+        spades[5] = new Vector2(148 * 8, 198 * 5);
+        spades[6] = new Vector2(148 * 8, 198 * 4);
+        spades[7] = new Vector2(148 * 8, 198 * 3);
+        spades[8] = new Vector2(148 * 8, 198 * 2);
+        spades[9] = new Vector2(148 * 8, 198 * 1);
+        spades[10] = new Vector2(148 * 0, 198 * 0);
+        spades[11] = new Vector2(148 * 1, 198 * 0);
+        spades[12] = new Vector2(148 * 2, 198 * 0);
+        spades[13] = new Vector2(148 * 3, 198 * 0);
 
-        spades[2] = new Vector2(740, 0);
+        hearts[1] = new Vector2(148 * 6, 198 * 2);
+        hearts[2] = new Vector2(148 * 7, 198 * 2);
+        hearts[3] = new Vector2(148 * 0, 198 * 1);
+        hearts[4] = new Vector2(148 * 1, 198 * 1);
+        hearts[5] = new Vector2(148 * 2, 198 * 1);
+        hearts[6] = new Vector2(148 * 3, 198 * 1);
+        hearts[7] = new Vector2(148 * 4, 198 * 1);
+        hearts[8] = new Vector2(148 * 5, 198 * 1);
+        hearts[9] = new Vector2(148 * 6, 198 * 1);
+        hearts[10] = new Vector2(148 * 2, 198 * 2);
+        hearts[11] = new Vector2(148 * 3, 198 * 2);
+        hearts[12] = new Vector2(148 * 4, 198 * 2);
+        hearts[13] = new Vector2(148 * 5, 198 * 2);
 
-        joker[1] = new Vector2(1184, 0);
+        clubs[1] = new Vector2(148 * 4, 198 * 5);
+        clubs[2] = new Vector2(148 * 5, 198 * 5);
+        clubs[3] = new Vector2(148 * 6, 198 * 5);
+        clubs[4] = new Vector2(148 * 7, 198 * 5);
+        clubs[5] = new Vector2(148 * 0, 198 * 4);
+        clubs[6] = new Vector2(148 * 1, 198 * 4);
+        clubs[7] = new Vector2(148 * 2, 198 * 4);
+        clubs[8] = new Vector2(148 * 3, 198 * 4);
+        clubs[9] = new Vector2(148 * 4, 198 * 4);
+        clubs[10] = new Vector2(148 * 0, 198 * 5);
+        clubs[11] = new Vector2(148 * 1, 198 * 5);
+        clubs[12] = new Vector2(148 * 2, 198 * 5);
+        clubs[13] = new Vector2(148 * 3, 198 * 5);
 
+        diamonds[1] = new Vector2(148 * 1, 198 * 3);
+        diamonds[2] = new Vector2(148 * 2, 198 * 3);
+        diamonds[3] = new Vector2(148 * 3, 198 * 3);
+        diamonds[4] = new Vector2(148 * 4, 198 * 3);
+        diamonds[5] = new Vector2(148 * 5, 198 * 3);
+        diamonds[6] = new Vector2(148 * 7, 198 * 3);
+        diamonds[8] = new Vector2(148 * 0, 198 * 2);
+        diamonds[9] = new Vector2(148 * 1, 198 * 2);
+        diamonds[10] = new Vector2(148 * 5, 198 * 4);
+        diamonds[11] = new Vector2(148 * 6, 198 * 4);
+        diamonds[12] = new Vector2(148 * 7, 198 * 4);
+        diamonds[13] = new Vector2(148 * 0, 198 * 3);
+
+        joker[0] = new Vector2(148 * 7, 198 * 1);
 
         CardTextureCoordinates[Suit.JOKER] = joker;
         CardTextureCoordinates[Suit.SPADES] = spades;
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     // Attributes
     public GameObject TheDeck;
     public GameObject Player;
+    public GameObject UI_Card;
+    public GameObject Grid;
+    public Sprite[] CardSpriteSheet;
     public List<GameObject> players;
+    private int hand;
+    private int curPlayer;
 
 
     // Start is called before the first frame update
@@ -20,8 +26,11 @@ public class GameManager : MonoBehaviour
         Deck deck = TheDeck.GetComponent<Deck>();
         deck.InitDeck(1, 2);
         deck.Shuffle();
-        int hand = 3;
+        hand = 3;
         Deal(hand);
+        curPlayer = 0;
+        updateUI();
+
     }
 
     // Update is called once per frame
@@ -41,6 +50,42 @@ public class GameManager : MonoBehaviour
             }
         }
 
-        deck.TheDiscardPile.Push(deck.TheDeck.Pop());
+        UpdateDiscardPile(deck.TheDeck.Pop());
+        
+    }
+
+    private void UpdateDiscardPile(Card card)
+    {
+        Deck deck = TheDeck.GetComponent<Deck>();
+        deck.TheDiscardPile.Push(card);
+        deck.UpdateDiscardPileImage(card);
+
+    }
+
+    private void updateUI()
+    {
+        for (int i = 0; i < hand; i++)
+        {
+            Suit suit = players[curPlayer].GetComponent<Player>().Hand[i].Suit;
+            int value = players[curPlayer].GetComponent<Player>().Hand[i].Value;
+
+            GameObject ui_card = Instantiate(UI_Card, new Vector3(0, 0, 0), Quaternion.identity);
+            ui_card.transform.SetParent(Grid.transform.GetChild(i).transform, true);
+            ui_card.transform.localScale = new Vector3(0.6f, 0.64f, 1.0f);
+            if (suit == Suit.JOKER)
+            {
+                ui_card.GetComponent<Image>().sprite = CardSpriteSheet[0];
+            }
+            else
+            {
+                ui_card.GetComponent<Image>().sprite = CardSpriteSheet[((int)suit - 1) * 13 + value];
+            }
+
+        }
+    }
+
+    private int GetNextPlayer()
+    {
+        return curPlayer++ % players.Count;
     }
 }
